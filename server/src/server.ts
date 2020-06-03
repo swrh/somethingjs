d23af355650a0763a8704056ab397133edff3ae4
Fernando Silveira <fsilveira@gmail.com>
2020-06-03 16:47:28 -0300

    Experimental routes to better understand Express

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,14 +2,59 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listing users...');
+app.use(express.json());
 
-    response.json([
-        'John',
-        'Jane',
-        'Bill',
-    ]);
+const users = [
+    {
+        name: 'John Doe',
+        email: 'john@doe.com',
+    },
+    {
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+    },
+    {
+        name: 'Baby Doe',
+        email: 'baby@doe.com',
+    },
+    {
+        name: 'Johnny Doe',
+        email: 'johnny@doe.com',
+    },
+    {
+        name: 'Janie Doe',
+        email: 'janie@doe.com',
+    },
+    {
+        name: 'Richard Roe',
+        email: 'richard@roe.com',
+    },
+    {
+        name: 'John Roe',
+        email: 'john@roe.com',
+    },
+    {
+        name: 'Jane Roe',
+        email: 'jane@roe.com',
+    },
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+    const filteredUsers = search ? users.filter(user => user.name.includes(search)) : users;
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    console.log(data);
+    return response.json(users[0]);
 });
 
 app.listen(3000);
