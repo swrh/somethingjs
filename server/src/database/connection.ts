3ee998c96e18cd7409ef99d2dccbb7ad3454fc4f
Fernando Silveira <fsilveira@gmail.com>
2020-06-04 07:06:33 -0300

    Import connection configuration from knexfile

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,11 +1,8 @@
 import knex from 'knex';
 import path from 'path';
 
-const connection = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: path.resolve(__dirname, 'database.sqlite3'),
-    },
-});
+import * as configuration from '../../knexfile';
+
+const connection = knex(configuration);
 
 export default connection;
